61de59dee2ed656ca4a0c9958a892175d0fb01e4
1ZeroStorm <162772235+1ZeroStorm@users.noreply.github.com>
2026-01-26 09:43:52 +0700

    update bubblesort

--- c++_testing/bubbleSort.cpp
+++ c++_testing/bubbleSort.cpp
@@ -4,25 +4,28 @@ using namespace std;
 int main(){
     
     
-    int unsorted[] = {10,1,9,2,8,3,7,4,6,5};
+    int unsorted[] = {10,1,9,2,8,3,7,4,6,5}; // {1,10,9,2,}
     int size = sizeof(unsorted) / sizeof(int);
     int temp;
-    int current = unsorted[0];
+    int current;
     int newnumber;
+    int saveafter;
+    
+    // look for unsorted
+    for (int j = 0;  j < size-1; j++){
+        cout << "comparing " << unsorted[j] << " with "<<unsorted[j+1] << endl;
+        if (unsorted[j] < unsorted[j+1]){
+            current = unsorted[j];
+            saveafter = unsorted[j+1];
+            break;
+        }
+    }
+
+    for (int i = 0; i < )
+
+
 
     
-
-
-    for (int j = 0;  j < size; j++){
-        if (unsorted[j] > unsorted[j+1]){
-            current = unsorted[j];
-            unsorted[j] = -1;
-            newnumber = unsorted[j+1];
-            unsorted[j] = newnumber;
-            unsorted[j+1] = current;
-        }
-        
-    }
 
     return 0;
 }
